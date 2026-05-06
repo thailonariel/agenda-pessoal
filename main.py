0bd7349b8cbeacc9a3e95795bcf77ec058bb63bb
Thailon Ariel Fritsch <thailonarielf@gmail.com>
2026-05-06 19:40:40 -0300

    Criação da interface inicial de cadstro de veículos com Flet

---
+++ main.py
@@ -0,0 +1,52 @@
+import flet as ft
+import sqlite3
+
+def main(page: ft.Page):
+    page.title = "Agenda Pessoal"
+    page.theme_mode = ft.ThemeMode.DARK
+    page.window_width = 400
+    page.window_height = 600
+
+    #Função para salvar os dados no banco de dados
+    def cadastrar_veiculo(e):
+        if not txt_modelo.value:
+            txt_modelo.error_text = "Por favor, digite o modelo"
+            page.update()
+            return
+        
+        conn = sqlite3.connect('data/agenda.db')
+        cursor = conn.cursor()
+        cursor.execute('''
+            INSERT INTO veiculos (modelo, marca, ano, cor, placa)
+            VALUES (?, ?, ?, ?, ?)
+        ''', (txt_modelo.value, txt_marca.value, txt_ano.value, txt_cor.value, txt_placa.value))
+        conn.commit()
+        conn.close()
+
+        # Limpar campos e avisar sucesso
+        txt_modelo.value = ""
+        txt_marca.value = ""
+        page.snack_bar = ft.SnackBar(ft.Text("Veículo cadastrado com sucesso!"))
+        page.snack_bar.open = True
+        page.update()
+
+    # Campos de entrada
+    txt_modelo = ft.TextField(label="Modelo (ex: Fiesta)")
+    txt_marca = ft.TextField(label="Marca (ex: Ford)")
+    txt_ano = ft.TextField(label="Ano", keyboard_type=ft.KeyboardType.NUMBER)
+    txt_cor = ft.TextField(label="Cor")
+    txt_placa = ft.TextField(label="Placa")
+    btn_salvar = ft.ElevatedButton("Cadastrar Veículo", on_click=cadastrar_veiculo)
+
+    # Adicionar na tela
+    page.add(
+        ft.Text("Cadastro de Veículo", size=30, weight=ft.FontWeight.BOLD),
+        txt_modelo,
+        txt_marca,
+        txt_ano,
+        txt_cor,
+        txt_placa,
+        btn_salvar
+    )
+
+ft.app(target=main)
